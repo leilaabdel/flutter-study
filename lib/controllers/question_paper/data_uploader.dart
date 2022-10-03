2042a0b763d778835027f999613634d8d312cf11
Leila Abdelrahman <leilaabdel123@gmail.com>
2022-10-03 13:18:05 -0400

    [ADD] Added data json reader

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class DataUploader extends GetxController {
@@ -7,7 +11,19 @@ class DataUploader extends GetxController {
     super.onReady();
   }
 
-  void uploadData() {
+  Future<void> uploadData() async {
     print("Uploading Data ...");
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString("AssetManifest.json");
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith("assets/DB/paper") && path.contains(".json"))
+        .toList();
+
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      print(stringPaperContent);
+    }
   }
 }
